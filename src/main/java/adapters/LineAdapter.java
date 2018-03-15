8fc30b997c6ac07bdefd68785f323d10aaa6c051
Ted Ginn <tginn@mail.greenriver.edu>
2018-03-14 21:04:44 -0700

    Initial commit

---
+++ src/main/java/adapters/LineAdapter.java
@@ -0,0 +1,71 @@
+package adapters;
+
+import drawing.IShape;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import shapes.Circle;
+import shapes.Line;
+
+public class LineAdapter implements IShape
+{
+    private Line line;
+
+    public LineAdapter(Line line)
+    {
+        this.line = line;
+    }
+
+    @Override
+    public IShape setThickness(double value)
+    {
+        return null;
+    }
+
+    @Override
+    public IShape setColor(Color value)
+    {
+        return null;
+    }
+
+    @Override
+    public IShape setFilled(boolean value)
+    {
+        return null;
+    }
+
+    @Override
+    public double getXCoordinate()
+    {
+        return 0;
+    }
+
+    @Override
+    public double getYCoordinate()
+    {
+        return 0;
+    }
+
+    @Override
+    public double getThickness()
+    {
+        return 0;
+    }
+
+    @Override
+    public Color getColor()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean getFilled()
+    {
+        return false;
+    }
+
+    @Override
+    public void drawShape(GraphicsContext graphics)
+    {
+
+    }
+}
